2716342a0c69874844221fde4e38179997987440
gmartino-sky <gonzolp@gmail.com>
2021-02-21 20:03:28 -0300

    correcion lista de usuarios windows v0.1.1

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.16
 require (
 	github.com/StackExchange/wmi v0.0.0-20190523213315-cbe66965904d // indirect
 	github.com/go-ole/go-ole v1.2.5 // indirect
+	github.com/iamacarpet/go-win64api v0.0.0-20200715182619-8cbc936e1a5a
 	github.com/klauspost/cpuid/v2 v2.0.3
 	github.com/shirou/gopsutil v3.21.1+incompatible
 	github.com/stretchr/testify v1.7.0 // indirect
--- main.go
+++ main.go
@@ -9,7 +9,9 @@ import (
 	"os/exec"
 	"os/user"
 	"strings"
+	"time"
 
+	wapi "github.com/iamacarpet/go-win64api"
 	. "github.com/klauspost/cpuid/v2"
 	"github.com/shirou/gopsutil/host"
 	"github.com/shirou/gopsutil/load"
@@ -21,6 +23,28 @@ var Users []string
 // PowerShell struct
 type PowerShell struct {
 	powerShell string
+}
+
+//GetUserListWindows lista los usuarios locales para windows
+func GetUserListWindows() {
+	users, err := wapi.ListLocalUsers()
+	if err != nil {
+		fmt.Printf("Error fetching user list, %s.\r\n", err.Error())
+		return
+	}
+
+	for _, u := range users {
+		fmt.Printf("%s (%s)\r\n", u.Username, u.FullName)
+		fmt.Printf("\tIs Enabled:                   %t\r\n", u.IsEnabled)
+		fmt.Printf("\tIs Locked:                    %t\r\n", u.IsLocked)
+		fmt.Printf("\tIs Admin:                     %t\r\n", u.IsAdmin)
+		fmt.Printf("\tPassword Never Expires:       %t\r\n", u.PasswordNeverExpires)
+		fmt.Printf("\tUser can't change password:   %t\r\n", u.NoChangePassword)
+		fmt.Printf("\tPassword Age:                 %.0f days\r\n", (u.PasswordAge.Hours() / 24))
+		fmt.Printf("\tLast Logon Time:              %s\r\n", u.LastLogon.Format(time.RFC850))
+		fmt.Printf("\tBad Password Count:           %d\r\n", u.BadPasswordCount)
+		fmt.Printf("\tNumber Of Logons:             %d\r\n", u.NumberOfLogons)
+	}
 }
 
 //New Crea una nueva sesion de ps
@@ -118,7 +142,8 @@ func ProcessList() {
 	// Valido SO y lista usuarios
 
 	if infoStat.OS == "windows" {
-		New()
+		//New()
+		GetUserListWindows()
 	} else {
 		GetUserListLinux()
 	}
